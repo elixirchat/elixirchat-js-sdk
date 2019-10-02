9c43620446efaada744e5805a9432e487f073b92
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2019-10-02 12:57:48 +0300

    Dedug info

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+
+printf "\n\n\n --- POSTINSTALL YO YO \n\n"
+printf " --- COMMAND: \n\n"
+echo $(command -v node-sass)
+printf " ^^^^^\n\n\n\n"
+
+sleep 4
+
 node-sass --recursive widget/DefaultWidget/styles/ --output dist/styles/
