909de65fbebe846657bcac23ad876ba95b287f61
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2019-10-02 13:41:53 +0300

    Potential fix for not installing node-sass

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-
-printf "\n\n\n --- POSTINSTALL YO YO 2 \n\n"
-printf " --- COMMAND: \n\n"
-echo $(command -v node-sass)
-printf " ^^^^^\n\n\n\n"
-
-sleep 4
-
 node-sass --recursive widget/DefaultWidget/styles/ --output dist/styles/
