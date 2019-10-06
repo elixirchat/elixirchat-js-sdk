2147b007dfca7bc8b8f5bf618f4b481fe4229433
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2019-10-06 17:17:24 +0300

    Change package import

---
+++ sdk/index.js
@@ -0,0 +1,7 @@
+/**
+ * This file is needed for non-TypeScript project to be able to use:
+ * import ElixirChat from 'elixirchat-js-sdk';
+ */
+
+import { ElixirChatWidget } from '../build/sdk';
+export default ElixirChatWidget;
--- widget/index.js
+++ widget/index.js
@@ -1,6 +1,6 @@
 /**
  * This file is needed for non-TypeScript project to be able to use:
- * import ElixirChatWidget from 'elixirchat-js-sdk';
+ * import ElixirChatWidget from 'elixirchat-js-sdk/widget';
  */
 
 import { ElixirChatWidget } from '../build/default-widget';
