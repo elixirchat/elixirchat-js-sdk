644cd7810c5018576db4b80f58d46aa7d2519c7c
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2019-10-07 12:48:12 +0300

    Change postinstall

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -7,3 +7,6 @@ const ElixirChat = require('../build/sdk').default;
 console.log('__ AFTER INSTALLED 1', { ElixirChat });
 export default ElixirChat;
 """ > dist/sdk.js
+
+parcel build widget/index.ts --out-dir build --out-file default-widget.js --no-source-maps --no-minify
+parcel build widget/index.ts --out-dir build --out-file default-widget.min.js --no-source-maps
