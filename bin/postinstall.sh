cda1e46087d9d4aa3442b94eee916084ce67ceb9
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2019-10-07 12:54:06 +0300

    Add debug info

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -8,5 +8,19 @@ console.log('__ AFTER INSTALLED 1', { ElixirChat });
 export default ElixirChat;
 """ > dist/sdk.js
 
+printf "---0\n\n\n\n---"
+
+ls -la node_modules
+
+printf "---1\n\n\n\n---"
+
+ls -la node_modules/node-sass
+
+printf "---2\n\n\n\n---"
+
+ls -la node_modules/parcel-bundler
+
+printf "---3\n\n\n\n---"
+
 parcel build widget/index.ts --out-dir build --out-file default-widget.js --no-source-maps --no-minify
 parcel build widget/index.ts --out-dir build --out-file default-widget.min.js --no-source-maps
