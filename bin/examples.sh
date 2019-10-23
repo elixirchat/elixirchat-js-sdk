8c1e0a60ce59e01e613ca902a3f92287be0c3150
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2019-10-23 12:39:30 +0300

    Change "npm run examples" to work w/ local server by default

--- bin/examples.sh
+++ bin/examples.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 # Usage:
-# npm run examples                        # Browse examples working with dev-server (dev-admin.elixir.chat/api)
-# npm run examples --local-server=true    # Browse examples working with local server (localhost:4000)
+# npm run examples                      # Browse examples working with local server (localhost:4000)
+# npm run examples --dev-server=true    # Browse examples working with dev-server (dev-admin.elixir.chat/api)
 
 export API_URL="http:\/\/localhost:4000"
 export SOCKET_URL="ws:\/\/localhost:4000\/socket"
 export BACKEND_STATIC_URL="http:\/\/localhost:4000"
 
-export IS_LOCAL_SERVER=$(npm config get local-server)
+export IS_DEV_SERVER=$(npm config get dev-server)
 
 replace_env_variables () {
-  sed -i '' -E -e "s/apiUrl: '[^']+'/apiUrl: '$API_URL'/" $1
-  sed -i '' -E -e "s/socketUrl: '[^']+'/socketUrl: '$SOCKET_URL'/" $1
-  sed -i '' -E -e "s/backendStaticUrl: '[^']+'/backendStaticUrl: '$BACKEND_STATIC_URL'/" $1
+  sed -i '' -E -e "s/apiUrl: '[^']+'/apiUrl: '$API_URL'/" "$1"
+  sed -i '' -E -e "s/socketUrl: '[^']+'/socketUrl: '$SOCKET_URL'/" "$1"
+  sed -i '' -E -e "s/backendStaticUrl: '[^']+'/backendStaticUrl: '$BACKEND_STATIC_URL'/" "$1"
 }
 
 update_examples_files () {
@@ -26,8 +26,12 @@ update_examples_files () {
   cp -rf build/examples/widget.html dist/build/examples/widget.html
   cp -rf build/examples/widget-private.html dist/build/examples/widget-private.html
 
-  if [ "$IS_LOCAL_SERVER" != "undefined" ];
+  if [ "$IS_DEV_SERVER" != "undefined" ];
     then
+      tput setaf 2
+      printf "\nRunning SDK with DEV-SERVER (dev-admin.elixir.chat/api)\n\n"
+      tput sgr0
+    else
       tput setaf 2
       printf "\nRunning SDK examples with LOCAL SERVER (localhost:4000)\n\n"
       tput sgr0
@@ -35,10 +39,6 @@ update_examples_files () {
       replace_env_variables "dist/build/examples/sdk.html"
       replace_env_variables "dist/build/examples/widget.html"
       replace_env_variables "dist/build/examples/widget-private.html"
-    else
-      tput setaf 2
-      printf "\nRunning SDK with DEV-SERVER (dev-admin.elixir.chat/api)\n\n"
-      tput sgr0
   fi
 }
 
