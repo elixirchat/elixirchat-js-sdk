24958629d52ae1d8c74cb4bdda0c11b053a7364c
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2019-11-21 17:53:41 +0300

    Update build script & rebuilt static

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+CURRENT_VERSION=$(node -p "require('./package.json').version")
 NEW_VERSION=$1
+
+if [ -n "$NEW_VERSION" ];
+  then
+    OUTPUT_VERSION=$NEW_VERSION
+  else
+    OUTPUT_VERSION=$CURRENT_VERSION
+fi
+
+
 DEFAULT_BUILD_DIR="build"
 BUILD_DIR=$(npm config get dir)
 
@@ -9,11 +19,12 @@ if [ -z "$BUILD_DIR" ] || [ "$BUILD_DIR" = "undefined" ]; then
 fi
 
 function write_version_to_sdk() {
-  js_code=";(function(){ if (typeof ElixirChat !== 'undefined') { ElixirChat.prototype.version = '$1'; } }())"
+  js_code="
+  ;(function(){ if (typeof ElixirChat !== 'undefined') { ElixirChat.prototype.version = '$1'; } }())"
   echo "$js_code" >> "$2"
 }
 
-printf "\nBuilding JS (version $NEW_VERSION) into '$BUILD_DIR' directory\n\n";
+printf "\nBuilding JS (version $OUTPUT_VERSION) into '$BUILD_DIR' directory\n\n";
 
 node-sass --recursive widget/DefaultWidget/styles/ --output dist/styles/
 
@@ -24,10 +35,8 @@ echo "" > dist/sdk.js
 parcel build sdk/index.ts --out-dir $BUILD_DIR --out-file sdk.js --no-source-maps --no-minify
 parcel build sdk/index.ts --out-dir $BUILD_DIR --out-file sdk.min.js --no-source-maps
 
-if [ -n "$NEW_VERSION" ]; then
-  write_version_to_sdk "$NEW_VERSION" $BUILD_DIR/sdk.js
-  write_version_to_sdk "$NEW_VERSION" $BUILD_DIR/sdk.min.js
-fi
+write_version_to_sdk "$OUTPUT_VERSION" $BUILD_DIR/sdk.js
+write_version_to_sdk "$OUTPUT_VERSION" $BUILD_DIR/sdk.min.js
 
 parcel build widget/index.ts --out-dir $BUILD_DIR --out-file default-widget.js --no-source-maps --no-minify
 parcel build widget/index.ts --out-dir $BUILD_DIR --out-file default-widget.min.js --no-source-maps
