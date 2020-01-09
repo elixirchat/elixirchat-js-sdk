588cda08376ada6cfd7645e4ddde1d53a8fd4e0a
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2020-01-09 17:46:33 +0300

    2.3.6

--- build/sdk.js
+++ build/sdk.js
@@ -7910,4 +7910,4 @@ var ElixirChat_1 = require("./ElixirChat");
 
 exports.default = ElixirChat_1.ElixirChat;
 },{"./ElixirChat":"Pqo8"}]},{},["QCba"], null)
-  ;(function(){ if (typeof ElixirChat !== 'undefined') { ElixirChat.prototype.version = '2.3.5'; } }())
+  ;(function(){ if (typeof ElixirChat !== 'undefined') { ElixirChat.prototype.version = '2.3.6'; } }())
