1500388bbcc32f93b2975ac7440256e8149a1560
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2020-01-15 20:38:01 +0300

    Remove debug files

--- log_argv.js
+++
@@ -1,5 +0,0 @@
-console.log('Type:', typeof process.env.npm_config_argv);
-console.log('Value:', process.env.npm_config_argv);
-console.log('Original npm args:', JSON.parse(process.env.npm_config_argv).original);
-
-throw new Error("aborting");
