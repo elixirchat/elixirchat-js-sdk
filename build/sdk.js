6c2a3ed0e83aa19d130c8501d49da53a10fe54fd
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2020-09-02 13:13:47 +0300

    Dev release 4.0.2.omnichannel@2

--- build/sdk.js
+++ build/sdk.js
@@ -18053,7 +18053,7 @@ function () {
 
     _classCallCheck(this, ElixirChat);
 
-    this.version = "4.0.2.omnichannel@1";
+    this.version = "4.0.2.omnichannel@2";
     this.config = {};
     this.joinRoomData = {};
     this.isInitialized = false;
