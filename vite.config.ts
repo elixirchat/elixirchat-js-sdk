a72a3e3bdecd424968ccbbe6ae9460a77d1be2d6
Akopyants <akopyants92@yandex.ru>
2026-03-18 00:31:12 +0300

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    plugins: [vue()],
     define: {
-      'process.env.NODE_ENV': JSON.stringify(mode === 'production' ? 'production' : 'development'),
+      'process.env.NODE_ENV': JSON.stringify(
+        mode === 'production' ? 'production' : 'development',
+      ),
       'process.env.ELIXIRCHAT_VERSION': JSON.stringify(env.ELIXIRCHAT_VERSION || ''),
     },
     server: {
