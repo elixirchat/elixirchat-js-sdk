3be12d88bc502746ed4504ccf18ddb6f1753b7d4
Akopyants <akopyants92@yandex.ru>
2026-03-20 13:56:45 +0300

    Create avatar.vue

---
+++ widget/VueWidget/components/avatar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+const { src = undefined, className = 'elixirchat-chat-avatar' } = defineProps<{
+  src?: string;
+  className?: string;
+}>();
+
+const hasError = ref(false);
+const showImage = computed(() => !!src && !hasError.value);
+</script>
+
+<template>
+  <img
+    v-if="showImage"
+    :class="className"
+    :src="src"
+    alt="Avatar"
+    @error="hasError = true"
+  >
+  <div
+    v-else
+    class="elixirchat-chat-avatar__system"
+    :class="[className]"
+  />
+</template>
