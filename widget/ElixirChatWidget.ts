0989e3ce32b46a0f72e2acb9a5a0dad738155f60
Akopyants <akopyants92@yandex.ru>
2026-03-27 16:37:05 +0300

    Update ElixirChatWidget.ts

--- widget/ElixirChatWidget.ts
+++ widget/ElixirChatWidget.ts
@@ -86,6 +86,12 @@ export class ElixirChatWidget extends ElixirChat {
 
   public widgetComponents: any = {};
   public widgetIFrameDocument: Document = {};
+  private storageListener = (event: StorageEvent): void => {
+    if (event.key === 'elixirchat-notifications-muted') {
+      const isMuted = event.newValue ? JSON.parse(event.newValue) : false;
+      this.toggleMute(isMuted);
+    }
+  };
 
   public appendWidget = (widgetConfig: IElixirChatWidgetConfig): void => {
     this.widgetConfig = widgetConfig || {};
@@ -132,16 +138,16 @@ export class ElixirChatWidget extends ElixirChat {
         this.navigateTo('chat');
       }
     });
+    this.removeCrossTabMuteListener();
     this.listenForCrossTabMuteChanges();
   }
 
   private listenForCrossTabMuteChanges(): void {
-    window.addEventListener('storage', (event: StorageEvent) => {
-      if (event.key === 'elixirchat-notifications-muted') {
-        const isMuted = event.newValue ? JSON.parse(event.newValue) : false;
-        this.toggleMute(isMuted);
-      }
-    });
+    window.addEventListener('storage', this.storageListener);
+  }
+
+  private removeCrossTabMuteListener(): void {
+    window.removeEventListener('storage', this.storageListener);
   }
 
   private setWidgetData(joinRoomData: IJoinRoomData) {
