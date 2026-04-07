632c49cf5ef00890d507edd1481335563b12badb
Akopyants <akopyants92@yandex.ru>
2026-04-07 13:25:23 +0300

    Revert "[SRV-24095] - Хантфлоу: отображение прочитанного сообщения"
    This reverts commit 46e74725ed18465a71fe4e388becb8bf878da238.

--- widget/DefaultWidget/ChatMessages.tsx
+++ widget/DefaultWidget/ChatMessages.tsx
@@ -474,35 +474,22 @@ class ChatMessagesComponent extends Component<IDefaultWidgetMessagesProps, IDefa
   initializeMessagesIntersectionObserver = () => {
     const observerParams = {
       root: this.scrollBlock.current,
-      threshold: [0, 0.25, 0.5, 0.75, 0.9],
+      threshold: 0.9, // triggers when 90% of message is within the viewport
     };
 
     this.messageVisibilityObserver = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
+      entries.map(entry => {
         const messageElement = entry.target;
-        const rootElement = this.scrollBlock.current;
 
-        if (!rootElement) {
-          return
-        };
-
-        const messageData = this.getDatasetValue(messageElement, 'messageData');
-        const rootHeight = rootElement.clientHeight;
-        const messageHeight = messageElement.clientHeight;
-        const visibleHeight = entry.intersectionRect.height;
-
-        const isLargeMessage = messageHeight > rootHeight;
-        
-        const isMessageVisibleEnough = isLargeMessage
-          ? visibleHeight >= rootHeight * 0.5
-          : entry.intersectionRatio >= 0.9;
-
-        this.setDatasetValues(messageElement, {
-          isMessageWithinViewport: isMessageVisibleEnough,
-        });
-
-        if (isMessageVisibleEnough && messageData.isUnread) {
-          this.onScrollOverUnreadMessage(messageData.id);
+        if (entry.isIntersecting) {
+          this.setDatasetValues(messageElement, { isMessageWithinViewport: true });
+          const messageData = this.getDatasetValue(messageElement, 'messageData');
+          if (messageData.isUnread) {
+            this.onScrollOverUnreadMessage(messageData.id);
+          }
+        }
+        else {
+          this.setDatasetValues(messageElement, { isMessageWithinViewport: false });
         }
       });
     }, observerParams);
